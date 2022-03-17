49d80af0cb49acfd620e33009251330883a7c6b3
h3llsize <53043705+h3llsize@users.noreply.github.com>
2022-03-17 17:44:43 +0500

    Update BotMainClass.java

--- src/main/java/org/fleamarket/bot/BotMainClass.java
+++ src/main/java/org/fleamarket/bot/BotMainClass.java
@@ -6,7 +6,6 @@ public class BotMainClass {
     public static void main(String[] args) {
         new ProjectMethods().start();
         System.setProperty("file.encoding", "UTF-8");
-        System.out.println("Я РУССКИЙ БЛЯТЬ! \\\\");
         TelegramBot telegramBot = new TelegramBot();
         telegramBot.execute();
     }
